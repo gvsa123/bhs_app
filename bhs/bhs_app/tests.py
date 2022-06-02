06b83a48df180f997fd7eb5f6ac497cdfe59f9ba
gvsa123 <gvsa123@gmail.com>
2022-06-01 18:43:55 -0600

    Implement Customer and Vehicle model tests.

--- bhs/bhs_app/tests.py
+++ bhs/bhs_app/tests.py
@@ -1,8 +1,18 @@
+from random import randint
 from django.test import TestCase
-from bhs_app.models import Customer
+from bhs_app.models import Customer, Vehicle
+'''
+# Sample objects
+Customer.objects.create(first_name='Noah', last_name='Gibbs', phone_number='14039236543',email='noahark@gmail.com',address='31 hamstring drive oklahoma')
+Customer(first_name='Liam', last_name='Watts', phone_number='14039876543',email='lwatt@gmail.ce',address='23 holy st los angeles')
+Vehicle(vin='WAUWGAFB0C0018472',year=2012,make='Audi',model='A4',mileage=234321)
+Vehicle(vin='JHMGE8G67AC003016',year=2010,make='Honda',model='Fit',mileage=163455)
+'''
 
-class CustomerTestCase(TestCase):
+class CustomerTestCaseWithoutVehicle(TestCase):
+    '''Create customers without vehicles'''
     def setUp(self) -> None:
+        # Create Customer objects
         Customer.objects.create(
             first_name = 'Girard',
             last_name = 'Aquino',
@@ -25,11 +35,71 @@ class CustomerTestCase(TestCase):
             address = '37 hollywood dr los angeles california'
         )
     
-    def test_create_new_customer(self):
-        '''Test creating a new customer without a vehicle'''
+    def test_create_new_customer(self) -> None:
+        '''Check created customer attributes'''
         girarda = Customer.objects.get(phone_number=14037144007)
         denisea = Customer.objects.get(phone_number=14036672458)
         deniser = Customer.objects.get(phone_number=18542766304)
+        
+        girarda.save()
+        denisea.save()
+        deniser.save()
+
         self.assertEqual(girarda.first_name, 'Girard')
         self.assertEqual(denisea.last_name, 'Aquino')
         self.assertEqual(deniser.address, '37 hollywood dr los angeles california')
+        self.assertIsNotNone(girarda.pk)
+        self.assertIsNotNone(denisea.pk)
+        self.assertIsNotNone(deniser.pk)
+        
+
+    def test_assign_vehicle_to_customer(self) -> None:
+        '''Test assigning a vehicle for created customer'''
+
+        # Create Customer objects
+        girarda = Customer.objects.get(phone_number=14037144007)
+        denisea = Customer.objects.get(phone_number=14036672458)
+        deniser = Customer.objects.get(phone_number=18542766304)
+
+        girarda.save()
+        denisea.save()
+        deniser.save()
+
+        # Create Vehicle objects
+        Vehicle.objects.create(
+            customer = girarda,
+            vin = 'JH4DA1745GS002661',
+            year = 1986,
+            make = 'Acura',
+            model = 'Integra',
+            mileage = randint(25000, 250000)
+        )
+        Vehicle.objects.create(
+            customer = denisea,
+            vin = 'WBAGH83401DP17574',
+            year = 2001,
+            make = 'BMW',
+            model = '7 Series',
+            mileage = randint(25000, 250000)
+        )
+        Vehicle.objects.create(
+            customer = deniser,
+            vin = '1FAHP3FNXAW231964',
+            year = 2010,
+            make = 'Ford',
+            model = 'Focus',
+            mileage = randint(25000, 250000)
+        )
+
+        v1 = Vehicle.objects.get(vin='JH4DA1745GS002661')
+        v2 = Vehicle.objects.get(vin='WBAGH83401DP17574')
+        v3 = Vehicle.objects.get(vin='1FAHP3FNXAW231964')
+
+        v1.save()
+        v2.save()
+        v3.save()
+
+        # Check that vehicle owner is correct.
+        self.assertEqual(v1.customer, girarda)
+        self.assertEqual(v2.customer, denisea)
+        self.assertEqual(v3.customer, deniser)
