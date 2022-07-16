652163ccdcf60323950ba8f6577cc1fa1b118fdd
gvsa123 <gvsa123@gmail.com>
2022-07-16 09:23:38 -0600

    Hyphenate urlpattern names.

--- bhs/bhs_app/urls.py
+++ bhs/bhs_app/urls.py
@@ -1,16 +1,19 @@
 from django.urls import path
 from . import views
-
+'''
+TODO:
+- redirect to view_customer_profile after search
+'''
 urlpatterns = [
     path('', views.index, name='index'),
     path('index/', views.index, name='index'),
     path('search/', views.search, name='search'),
     path('thanks/', views.thanks, name='thanks'),
-    path('view_customers/', views.view_customers, name='view_customers'),
-    path('create_new_customer/', views.create_new_customer, name='create_new_customer'),
-    path('search/view_customer_profile/<int:customer_id>/', views.view_customer_profile, name='view_customer_profile'),
-    path('view_customers/view_customer_profile/<int:customer_id>/', views.view_customer_profile, name='view_customer_profile'),
-    path('view_customers/view_customer_profile/<int:customer_id>/create_new_vehicle/', views.create_new_vehicle, name='create_new_vehicle'),
-    path('view_customers/view_customer_profile/<int:customer_id>/create_new_comment/', views.create_new_comment, name='create_new_comment'),
-    path('view_customers/view_customer_profile/<int:customer_id>/create_new_repair_order/', views.create_new_repair_order, name='create_new_repair_order'),
+    path('view_customers/', views.view_customers, name='view-customers'),
+    path('create_new_customer/', views.create_new_customer, name='create-new-customer'),
+    path('search/view_customer_profile/<int:customer_id>/', views.view_customer_profile, name='view-customer-profile'),
+    path('view_customers/view_customer_profile/<int:customer_id>/', views.view_customer_profile, name='view-customer_profile'),
+    path('view_customers/view_customer_profile/<int:customer_id>/create_new_vehicle/', views.create_new_vehicle, name='create-new-vehicle'),
+    path('view_customers/view_customer_profile/<int:customer_id>/create_new_comment/', views.create_new_comment, name='create-new-comment'),
+    path('view_customers/view_customer_profile/<int:customer_id>/create_new_repair_order/', views.create_new_repair_order, name='create-new-repair-order'),
 ]
