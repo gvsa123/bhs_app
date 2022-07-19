0868abef6d2ea1d0752a65611f5123931347d24a
gvsa123 <gvsa123@gmail.com>
2022-07-19 10:24:19 -0600

    Created view_vehicle_info (route and all) for relevant vehicle. Removed unnecessary information from view_customers and strealined view_customer_profile to show all registered vehicles for customer.

--- bhs/bhs_app/urls.py
+++ bhs/bhs_app/urls.py
@@ -13,6 +13,7 @@ urlpatterns = [
     path('create_new_customer/', views.create_new_customer, name='create-new-customer'),
     path('search/view_customer_profile/<int:customer_id>/', views.view_customer_profile, name='view-customer-profile'),
     path('view_customers/view_customer_profile/<int:customer_id>/', views.view_customer_profile, name='view-customer_profile'),
+    path('view_customers/view_customer_profile/<int:customer_id>/view_vehicle_info/<str:vehicle_vin>/', views.view_vehicle_info, name='view-vehicle-info'),
     path('view_customers/view_customer_profile/<int:customer_id>/create_new_vehicle/', views.create_new_vehicle, name='create-new-vehicle'),
     path('view_customers/view_customer_profile/<int:customer_id>/create_new_comment/', views.create_new_comment, name='create-new-comment'),
     path('view_customers/view_customer_profile/<int:customer_id>/create_new_repair_order/', views.create_new_repair_order, name='create-new-repair-order'),
--- bhs/bhs_app/views.py
+++ bhs/bhs_app/views.py
@@ -6,27 +6,34 @@ from .forms import CustomerForm, VehicleForm, RepairOrderForm, CommentsForm
 from . import models
 from . import managers
 
+
 def index(request):
     return render(request, 'bhs_app/index.html')
+
 
 def login(request):
     return render(request, 'bhs_app/login.html')
 
+
 @login_required(login_url='login')
 def create_new_customer(request):
     '''Creae a new customer'''
+
     if request.method == 'POST':
         form_customer = CustomerForm(request.POST)
+
         if form_customer.is_valid():
             form_customer.save(commit=True)
             return HttpResponseRedirect('/thanks/')
     else:
         form_customer = CustomerForm()
+
     return render(
         request,
         'bhs_app/create_new_customer.html',
         {'form_customer': form_customer}
     )
+
 
 @login_required(login_url='login')
 def create_new_vehicle(request, customer_id):
@@ -52,13 +59,21 @@ def create_new_vehicle(request, customer_id):
         {'form_vehicle': form_vehicle, 'data': customer_id}
     )
 
+
 @login_required(login_url='login')
 def create_new_repair_order(request, customer_id):
     '''Create a repair order
     TODO:
-    - fix -> NOT NULL constraint customer_id
+    -   generate list of vins associated with customer_id and limit choices
+        in RepairOrder
     '''
-    customer = models.Customer.objects.all().filter(pk=customer_id)
+    current_customer = models.Customer.objects.all().filter(pk=customer_id)
+    current_vehicles = managers.AllVehicles.all_vehicles.values()
+    list_of_owned_vehicles = []
+
+    for _ in current_vehicles:
+        if _['customer_id'] == customer_id:
+            list_of_owned_vehicles.append(_)
 
     if request.method == 'POST':
         form_repair_order = RepairOrderForm(request.POST)
@@ -69,8 +84,9 @@ def create_new_repair_order(request, customer_id):
         form_repair_order = RepairOrderForm()
     return render(
         request, 'bhs_app/create_new_repair_order.html',
-        {'form_repair_order': form_repair_order,'data': customer}
+        {'form_repair_order': form_repair_order,'data': current_customer}
     )
+
 
 @login_required(login_url='login')
 def create_new_comment(request, customer_id):
@@ -92,13 +108,17 @@ def create_new_comment(request, customer_id):
         {'form_comment': form_comment,'data': customer}
     )
 
+
 @login_required(login_url='login')
 def search(request):
     '''Search the database
     TODO:
     - default outputs all() due to managers.py; maybe output max 10 sorted by
     created recently?
-    - catch non-GET request method and throw error? for security?
+    - catch non-GET request method and throw error? for security? how? why?
+
+    BUG:
+    - view_customer_profile link breaks when coming from search/
     '''
     if request.method == 'GET':
         q = request.GET.get('q')
@@ -108,30 +128,57 @@ def search(request):
             data = models.Customer.objects.all().filter(phone_number__icontains=q)
     return render(request, 'bhs_app/search.html', {'data': data})
 
+
 @login_required(login_url='login')
 def view_customers(request):
     '''Display data on webpage
     TODO:
     - limit number of row items
     '''
+
     all_customer_data = managers.AllCustomers.all_customers
     return render(request, 'bhs_app/view_customers.html',
                  {'data': list(all_customer_data)})
 
+
 @login_required(login_url='login')
 def view_customer_profile(request, customer_id):
     '''Display customer profile page'''
+    
+    # redudant among functions; move to function
     customer = models.Customer.objects.all().filter(pk=customer_id)
     customer_vehicles = managers.AllVehicles.all_vehicles.filter(customer=customer[0])
+
     return render(
         request,
         'bhs_app/view_customer_profile.html',
         {
             'data_customer': customer,
             'data_vehicle': customer_vehicles,
-            'customer_id': customer_id
+            'customer_id': customer_id,
+            # 'vehicle_vin': vehicle_vin
         }
     )
+
+
+@login_required(login_url='login')
+def view_vehicle_info(request, customer_id, vehicle_vin):
+    """Display vehicle information and asssociated repair orders"""
+    
+    customer = models.Customer.objects.all().filter(pk=customer_id)
+    vehicle = managers.AllVehicles.all_vehicles.filter(vin=vehicle_vin)
+    
+    return render(
+        request,
+        'bhs_app/view_vehicle_info.html',
+        {
+            'data_customer': customer,
+            'data_vehicle': vehicle,
+            'customer_id': customer_id,
+            'vehicle_vin': vehicle_vin
+        }
+    )
+
 
 @login_required(login_url='login')
 def thanks(request):
