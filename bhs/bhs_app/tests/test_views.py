9dd307f1a5075b35dc281b68652855d9ff3b57a8
gvsa123 <gvsa123@gmail.com>
2022-08-06 15:17:45 -0600

    Create test for view_repair_info url.

--- bhs/bhs_app/tests/test_views.py
+++ bhs/bhs_app/tests/test_views.py
@@ -3,6 +3,7 @@ from bhs_app import urls
 
 import requests
 
+
 class URLPatternTest(TestCase):
     '''Test all url patterns return 200. Server has to be running.'''
     def test_all_urls(self) -> None:
@@ -17,27 +18,31 @@ class URLPatternTest(TestCase):
             if r.status_code == 200:
                 self.assertEqual(r.status_code, 200)
                 print(f"{temp_url} ---> {r.status_code} {r.reason}")
-            
+
             # handle 404 caused by url parameters: '<int:customer_id>'
             elif r.status_code == 404:
-                    f_url = []
-                    t_url = []
-                    n_url = []
+                f_url = []
+                t_url = []
+                n_url = []
 
-                    for u in temp_url.split('/'):
-                        n_url.append(u)
+                for u in temp_url.split('/'):
+                    n_url.append(u)
 
-                    for n in n_url:
-                        if n == '<int:customer_id>':
-                            # test url parameter actually fails
-                            self.assertEqual(r.reason, 'Not Found')
-                            n = n.replace('<int:customer_id>', str(1))
-                        t_url.append(n)
-                    
-                    # process url parameter
-                    f_url.append('/'.join(t_url))
-                    
-                    # retest
-                    r = requests.get(f_url[0])
-                    self.assertEqual(r.status_code, 200)
-                    print(f"{f_url[0]} ---> {r.status_code} {r.reason}")
+                for n in n_url:
+                    if n == '<int:customer_id>':
+                        # test url parameter actually fails
+                        self.assertEqual(r.reason, 'Not Found')
+                        n = n.replace('<int:customer_id>', str(1))
+                    if n == '<int:ro_num>':
+                        self.assertEqual(r.reason, 'Not Found')
+                        n = n.replace('<int:ro_num>', str(1))
+
+                    t_url.append(n)
+
+                # process url parameter
+                f_url.append('/'.join(t_url))
+
+                # retest
+                r = requests.get(f_url[0])
+                self.assertEqual(r.status_code, 200)
+                print(f"{f_url[0]} ---> {r.status_code} {r.reason}")
