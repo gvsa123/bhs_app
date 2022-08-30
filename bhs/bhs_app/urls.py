1507583014ca324883eeb82062d483a5f1375362
gvsa123 <gvsa123@gmail.com>
2022-08-29 22:27:24 -0600

    Temporary fix for url redirection problem.

--- bhs/bhs_app/urls.py
+++ bhs/bhs_app/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from . import views
 """
 TODO:
-- redirect to view_customer_profile after search.
-- why did i use underscores? ~ convert to dashes.
+- Will need to figure out how redirection works
+  to avoid scaling issues with this schema. Redirect
+  to view_customer_profile after search.
 """
 urlpatterns = [
     path('', views.index, name='index'),
@@ -12,7 +13,13 @@ urlpatterns = [
     path('thanks/', views.thanks, name='thanks'),
     path('view_customers/', views.view_customers, name='view-customers'),
     path('create_new_customer/', views.create_new_customer, name='create-new-customer'),
+
+    # Redirect problem evident in repetition
     path('search/view_customer_profile/<int:customer_id>/', views.view_customer_profile, name='view-customer-profile'),
+    path('search/view_customer_profile/<int:customer_id>/view_vehicle_info/<str:vehicle_vin>/', views.view_vehicle_info, name='view-vehicle-info'),
+    path('search/view_customer_profile/<int:customer_id>/view_vehicle_info/<str:vehicle_vin>/create_new_repair_order/', views.create_new_repair_order, name='create-new-repair-order'),
+    path('search/view_customer_profile/<int:customer_id>/view_vehicle_info/<str:vehicle_vin>/view_repair_order_info/<int:ro_num>/', views.view_repair_order, name='view-repair-order-info'),
+    path('search/view_customer_profile/<int:customer_id>/view_vehicle_info/<str:vehicle_vin>/view_repair_order_info/<int:ro_num>/create_new_comment/', views.create_new_comment, name='create-new-comment'),
     path('view_customers/view_customer_profile/<int:customer_id>/', views.view_customer_profile, name='view-customer_profile'),
     path('view_customers/view_customer_profile/<int:customer_id>/view_vehicle_info/<str:vehicle_vin>/', views.view_vehicle_info, name='view-vehicle-info'),
     path('view_customers/view_customer_profile/<int:customer_id>/view_vehicle_info/<str:vehicle_vin>/create_new_repair_order/', views.create_new_repair_order, name='create-new-repair-order'),
