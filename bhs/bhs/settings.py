1234752bbcceb79c20cbb11674bbee1ff0f1a798
gvsa123 <gvsa123@gmail.com>
2022-09-03 11:24:50 -0600

    Apply some basic styling.

--- bhs/bhs/settings.py
+++ bhs/bhs/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -118,7 +119,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+# This is the statistics file context path for url requests.
 STATIC_URL = 'static/'
+
+# The static file directory inside app.
+STATICFILES_DIR = [
+    os.path.join(BASE_DIR, 'static')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
